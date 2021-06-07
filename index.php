7f3a24b07ad153469c882ab98e347c039586707e
samuelfernandes <samueldiablo73@gmail.com>
2021-06-06 22:13:33 -0300

    modificacoes na nav

--- index.php
+++ index.php
@@ -30,28 +30,26 @@ $filmes = $database->getReference('filmes')->getSnapshot();
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style-index.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
     <title>Site Web</title>
 </head>
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light shadow-sm p-3 mb-5 bg-white rounded">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-                <strong class="txt-margin"> Recomenda Filmes</strong> 
-            </a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a href="login.php" class="nav-link"> <img src="img/logout.svg" alt="" class="img-logout"> </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <header id="header">
+        <a id="logo" href="index.php">Recomenda Filmes</a>
+        <nav id="nav">
+            <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
+                <span id="hamburger"></span>
+            </button>
+            <ul id="menu" role="menu">
+                <li><a href="/">Sobre</a></li>
+                <li><a href="/">Produtos</a></li>
+                <li><a href="/">Portfólio</a></li>
+                <li><a href="/">Contato</a></li>
+                <a href="login.php"><img src="img/logout.svg" alt="" class="img-logout"></a>
+            </ul>
+        </nav>
+    </header>
 
     <?php foreach ($filmes->getValue() as $filme) : ?>
         <div class="container">
@@ -69,6 +67,8 @@ $filmes = $database->getReference('filmes')->getSnapshot();
     <?php endforeach; ?>
 
 </body>
+
+<script src="script.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
